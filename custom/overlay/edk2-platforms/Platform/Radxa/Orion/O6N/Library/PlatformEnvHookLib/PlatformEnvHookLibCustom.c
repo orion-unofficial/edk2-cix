bd593dc7fc65bd28de326cd22bddd009c448c759
Orion Unofficial <orion-unofficial@protonmail.com>
2026-03-31 03:48:27 +0100

    build: fix custom UART3 debug pinmux

--- custom/overlay/edk2-platforms/Platform/Radxa/Orion/O6N/Library/PlatformEnvHookLib/PlatformEnvHookLibCustom.c
+++ custom/overlay/edk2-platforms/Platform/Radxa/Orion/O6N/Library/PlatformEnvHookLib/PlatformEnvHookLibCustom.c
@@ -13,4 +13,41 @@
 #endif
 #endif
 
+#define InitPinmux ImportedInitPinmux
 #include "../../../../../../../../../src/edk2-platforms/Platform/Radxa/Orion/O6N/Library/PlatformEnvHookLib/PlatformEnvHookLib.c"
+#undef InitPinmux
+
+STATIC
+VOID
+ApplyDebugUart3PinMuxOverride (
+  VOID
+  )
+{
+#if DEBUG_ON_UART3
+  UINTN  Index;
+
+  //
+  // The public pad definitions describe UART3_TXD/RXD as raw function 0, with
+  // function 1 mapping the same pads to GPIO105/GPIO106. Force the imported
+  // O6N pinmux table to select the UART function when DEBUG_ON_UART3 is enabled.
+  //
+  for (Index = 0; Index < ARRAY_SIZE (PinMuxCfgTable); Index++) {
+    if ((PinMuxCfgTable[Index].Offset == IO_S0_UART3_TXD) ||
+        (PinMuxCfgTable[Index].Offset == IO_S0_UART3_RXD))
+    {
+      PinMuxCfgTable[Index].FuncSel = IO_FUNC00;
+    }
+  }
+#endif
+}
+
+EFI_STATUS
+EFIAPI
+InitPinmux (
+  IN OUT ENV_HOOK_PARAMS_DATA_BLOCK  *ConfigData
+  )
+{
+  ApplyDebugUart3PinMuxOverride ();
+
+  return ImportedInitPinmux (ConfigData);
+}
